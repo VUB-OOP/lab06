178494069028eb61e4fd43f9aca78a05c4602ae9
Krunoslav Husak <khusak@vub.hr>
2024-11-10 17:35:06 +0000

    Korištenje klase Student

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include "student.h"
 
 int main() {
+  Datum datum(29, 2, 2024);
+
+  Adresa adresa("Smiciklasova", 1, "Zagreb");
+
+  Student student("Hrvoje", "Horvat", 123, datum, adresa);
+  student.prikaziPodatke();
 }
