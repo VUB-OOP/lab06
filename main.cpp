84d07b303943760a4525a3b2639c810144401407
Krunoslav Husak <khusak@vub.hr>
2024-11-10 17:43:13 +0000

    Dodavanje primjera stvaranja studenta s anonimnim objektima

--- main.cpp
+++ main.cpp
@@ -7,5 +7,8 @@ int main() {
   Adresa adresa("Smiciklasova", 1, "Zagreb");
 
   Student student("Hrvoje", "Horvat", 123, datum, adresa);
+  
+  // dodatan primjer stvaranja studenta s anonimnim objektima
+  Student student2("Ivan", "Ivic", 456, Datum(15, 1, 2024), Adresa("Ilica", 2, "Zagreb"));
   student.prikaziPodatke();
 }
