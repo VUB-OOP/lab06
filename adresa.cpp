de3f6d002b28f91eb7100f1426d457aa4fb63b0f
Krunoslav Husak <khusak@vub.hr>
2024-11-10 17:19:00 +0000

    Definirajte potrebne konstruktore i metode kako bi se postavili i dohvatili podaci o adresi

--- adresa.cpp
+++ adresa.cpp
@@ -1,1 +1,38 @@
 #include "adresa.h"
+
+Adresa::Adresa(std::string ulica, int broj, std::string grad) {
+  setUlica(ulica);
+  setBroj(broj);
+  setGrad(grad);
+}
+
+std::string Adresa::getUlica() {
+  return ulica;
+}
+int Adresa::getBroj() {
+  return broj;
+}
+std::string Adresa::getGrad() {
+  return grad;
+}
+
+Adresa& Adresa::setUlica(std::string ulica) {
+  this->ulica = ulica;
+  return *this;
+}
+Adresa& Adresa::setBroj(int broj) {
+  if (broj < 1) {
+    this->broj = 1;
+  } else {
+    this->broj = broj;
+  }
+  return *this;
+}
+Adresa& Adresa::setGrad(std::string grad) {
+  this->grad = grad;
+  return *this;
+}
+
+std::string Adresa::toString() {
+  return ulica + " " + std::to_string(broj) + ", " + grad;
+}
--- adresa.h
+++ adresa.h
@@ -1,6 +1,19 @@
 #include <string>
 
 class Adresa {
+public:
+  Adresa(std::string ulica, int broj, std::string grad);
+
+  std::string getUlica();
+  int getBroj();
+  std::string getGrad();
+
+  Adresa& setUlica(std::string ulica);
+  Adresa& setBroj(int broj);
+  Adresa& setGrad(std::string grad);
+
+  std::string toString();
+
 private:
   std::string ulica;
   int broj;
